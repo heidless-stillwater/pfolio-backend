5e669699189d936e3dbd9bf5b78b98feff419ff1
heidless <lockhart.r@gmail.com>
2024-11-21 10:06:10 +0000

    update & deploy to heidless09@gmail.com re-try

--- config/settings.py
+++ config/settings.py
@@ -25,15 +25,11 @@ elif os.environ.get('GOOGLE_CLOUD_PROJECT', None):
     project_id = os.environ.get('GOOGLE_CLOUD_PROJECT')
     secret_id = os.environ.get('GCP_SECRET_NAME')
     version_id = 1
-        
     client = secretmanager.SecretManagerServiceClient()
     settings_name = os.environ.get('GCP_SECRET_NAME', 'pfolio-backend-secret-0')
     name = f'projects/{project_id}/secrets/{settings_name}/versions/latest'
     payload = client.access_secret_version(name=name).payload.data.decode('UTF-8')
-    
     env.read_env(io.StringIO(payload))
-
-    
 else:
     raise Exception('No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.')
 
